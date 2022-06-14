be8bf745734f45eca624a4be0f126eb4f56b0787
Combonary <pascaladjaero@gmail.com>
2022-06-14 18:32:08 -0500

    Initial commit
    dependency setup

---
+++ app/src/main/java/com/example/trendingmovies/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.trendingmovies.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.trendingmovies.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
